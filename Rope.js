33374b63ca51ef19ae664791eafc83dea4a961d1
yugandharabhosale <87601266+yugandharabhosale@users.noreply.github.com>
2021-08-29 13:22:31 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,17 @@
+class Rope {
+  constructor(bodyA, pointB) {
+    var options = {
+      bodyA: bodyA,
+      pointB: pointB,
+      stiffness: 1.2,
+      length: 450,
+    };
+
+    this.pointB = pointB;
+    this.rope = Constraint.create(options);
+    World.add(world, this.rope);
+  }
+    
+  display() {
+  }
+}
